78eae396ac1c6d5fa013096c660ce5e87b2459a7
FuuKowatty <miedzio.2000@gmail.com>
2023-12-05 20:19:12 +0100

    refactor: createOffer - when creates offer it generates id and createdAt in repository now to better implement database

--- src/main/java/pl/bartoszmech/domain/offer/OfferConfiguration.java
+++ src/main/java/pl/bartoszmech/domain/offer/OfferConfiguration.java
@@ -1,7 +1,7 @@
 package pl.bartoszmech.domain.offer;
 
 public class OfferConfiguration {
-    OfferFacade createForTests(OfferValidator validator, OfferRepository repository, HashGenerator generator, OfferFetcher fetcher) {
-        return new OfferFacade(validator, repository, generator, fetcher);
+    OfferFacade createForTests(OfferValidator validator, OfferRepository repository, OfferFetcher fetcher) {
+        return new OfferFacade(validator, repository, fetcher);
     }
 }
