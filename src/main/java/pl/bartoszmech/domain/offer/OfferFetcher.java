53c0f6790443040988ea3abf18386b517abbf67f
FuuKowatty <miedzio.2000@gmail.com>
2023-12-05 17:01:59 +0100

    style: change snake case to camel case in OfferDto

---
+++ src/main/java/pl/bartoszmech/domain/offer/OfferFetcher.java
@@ -0,0 +1,2 @@
+package pl.bartoszmech.domain.offer;public interface OfferFetcher {
+}
---
+++ src/test/java/pl/bartoszmech/domain/offer/OfferFetcherTestImpl.java
@@ -0,0 +1,12 @@
+package pl.bartoszmech.domain.offer;
+
+import pl.bartoszmech.domain.offer.dto.OfferDto;
+
+import java.util.Set;
+
+public class OfficeFetcherTestImpl implements OfferFetcher{
+    @Override
+    public Set<OfferDto> fetch() {
+        
+    }
+}
