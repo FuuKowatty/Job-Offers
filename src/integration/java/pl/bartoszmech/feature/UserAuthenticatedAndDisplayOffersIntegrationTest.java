883a66154c6ee6df241b1e13ed55622bc8429656
FuuKowatty <miedzio.2000@gmail.com>
2023-12-06 14:55:23 +0100

    test: configure database for test container v2

---
+++ src/integration/java/pl/bartoszmech/feature/UserAuthenticatedAndDisplayOffersIntegrationTest.java
@@ -0,0 +1,12 @@
+package pl.bartoszmech.feature;
+
+
+import org.junit.jupiter.api.Test;
+import pl.bartoszmech.BaseIntegrationTest;
+
+public class UserAuthenticatedAndDisplayOffersIntegrationTest extends BaseIntegrationTest {
+    @Test
+    public void should_be_authenticated_to_display_offer() {
+        //
+    }
+}
--- src/main/java/pl/bartoszmech/JobOffersApplication.java
+++ src/main/java/pl/bartoszmech/JobOffersApplication.java
@@ -1,4 +1,4 @@
-package pl.bartoszmech.domain;
+package pl.bartoszmech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
