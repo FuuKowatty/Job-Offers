148c796e4259ccef5452d7cb4f5e1e1742881328
FuuKowatty <miedzio.2000@gmail.com>
2023-12-06 22:27:45 +0100

    test: implement wiremock v1 - use and test rest template

--- src/test/java/pl/bartoszmech/domain/offer/OfferFetcherTestImpl.java
+++ src/test/java/pl/bartoszmech/domain/offer/OfferFetcherTestImpl.java
@@ -1,17 +1,17 @@
 package pl.bartoszmech.domain.offer;
 
+import pl.bartoszmech.domain.offer.dto.OfferApiDto;
 import pl.bartoszmech.domain.offer.dto.OfferDto;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class OfferFetcherTestImpl implements OfferFetcher{
     @Override
-    public List<OfferDto> fetch() {
+    public List<OfferApiDto> handleFetchOffers() {
         return List.of(
-                OfferDto.builder().title("Junior Java Developer").company("randomCompany").salary("1000EURO").jobUrl("https://example.com").build(),
-                OfferDto.builder().title("Junior Java Developer").company("randomCompany").salary("800EURO").jobUrl("https://example1.com").build(),
-                OfferDto.builder().title("Junior Java Developer").company("randomCompany").salary("2000EURO").jobUrl("https://example2.com").build()
+                OfferApiDto.builder().title("Junior Java Developer").company("randomCompany").salary("1000EURO").jobUrl("https://example.com").build(),
+                OfferApiDto.builder().title("Junior Java Developer").company("randomCompany").salary("800EURO").jobUrl("https://example1.com").build(),
+                OfferApiDto.builder().title("Junior Java Developer").company("randomCompany").salary("2000EURO").jobUrl("https://example2.com").build()
         );
     }
 }
