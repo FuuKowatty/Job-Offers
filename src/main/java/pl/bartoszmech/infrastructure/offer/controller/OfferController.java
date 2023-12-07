ad365c9d57aeb5cfcc4282aaa8517870f88048d3
FuuKowatty <miedzio.2000@gmail.com>
2023-12-07 23:50:09 +0100

    refactor: delete manual validation impl

--- src/main/java/pl/bartoszmech/infrastructure/offer/controller/OfferController.java
+++ src/main/java/pl/bartoszmech/infrastructure/offer/controller/OfferController.java
@@ -1,15 +1,17 @@
 package pl.bartoszmech.infrastructure.offer.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.bartoszmech.domain.offer.OfferFacade;
-import pl.bartoszmech.domain.offer.dto.OfferApiDto;
-import pl.bartoszmech.domain.offer.dto.OfferDto;
+import pl.bartoszmech.domain.offer.dto.OfferRequest;
+import pl.bartoszmech.domain.offer.dto.OfferResponse;
 
-import java.net.URI;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/offers")
@@ -18,26 +20,18 @@ public class OfferController {
     OfferFacade offerFacade;
 
     @GetMapping
-    public ResponseEntity<List<OfferDto>> findOffers() {
-        return ResponseEntity.ok(offerFacade.listOffers());
+    public ResponseEntity<List<OfferResponse>> findOffers() {
+        return ResponseEntity.status(OK).body(offerFacade.listOffers());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OfferDto> findOfferById(@PathVariable("id") String id) {
-        return ResponseEntity.ok(offerFacade.getOfferById(id));
+    public ResponseEntity<OfferResponse> findOfferById(@PathVariable("id") String id) {
+        return ResponseEntity.status(OK).body(offerFacade.getOfferById(id));
     }
 
     @PostMapping
-    public ResponseEntity<OfferDto> createOffer(@RequestBody OfferApiDto inputOffer ) {
-        OfferDto offer = offerFacade.createOffer(inputOffer);
-        return ResponseEntity.created(getLocation(offer.id())).body(offer);
+    public ResponseEntity<OfferResponse> createOffer(@RequestBody @Valid OfferRequest inputOffer ) {
+        return ResponseEntity.status(CREATED).body(offerFacade.createOffer(inputOffer));
     }
 
-    private URI getLocation(String id) {
-        return ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(id)
-                .toUri();
-    }
 }
