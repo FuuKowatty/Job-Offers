f674b3f8fa8cddfa71070eb6b6a6f2daad43da17
FuuKowatty <miedzio.2000@gmail.com>
2023-12-10 12:20:31 +0100

    feat: implement MongoRepository

--- src/main/java/pl/bartoszmech/domain/accountidentifier/AccountIdentifierRepository.java
+++ src/main/java/pl/bartoszmech/domain/accountidentifier/AccountIdentifierRepository.java
@@ -1,10 +1,11 @@
 package pl.bartoszmech.domain.accountidentifier;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-interface AccountIdentifierRepository {
-    User save(User user);
+@Repository
+public interface AccountIdentifierRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
-    User register(User user);
-    boolean isExistsByUsername(String username);
 }
